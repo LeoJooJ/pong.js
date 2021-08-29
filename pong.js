11939b5c7caf13b60c64faffebabf013a373eaba
Leo_JooJ <leonard0jorg34@gmail.com>
2021-08-29 13:09:51 -0300

    I make the basic structure of index and pong.js

---
+++ pong.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById('main-canvas');//creating a variable to canvas object.
+const ctx = canvas.getContext('2d');//getting the context of canvas.
+
+function renderScreen() {
+    //Here's goes the game code.
+    
+}setInterval(renderScreen, 100);
